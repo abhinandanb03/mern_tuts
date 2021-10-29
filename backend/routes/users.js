872f0721f5dad3141f6a042a7e2b3cf76cba73b4
abhinandanb03 <abhinandanb03@gmail.com>
2021-10-29 11:20:43 +0530

    integrated node and react

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,7 +13,7 @@ router.route('/add').post((req,res) => {
 
     newUser.save()
         .then(() => res.json('User added'))
-        .catch(err => res.status(400).json("error: " + err))
+        .catch(err => alert.res.status(400).json("error: " + err))
 });
 
 module.exports = router;
